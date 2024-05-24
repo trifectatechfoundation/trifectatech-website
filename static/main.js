06ca23d0f1c2ee64eb8837a005c0f0122dcfa69e
Erwin <erwin@grafische-republiek.com>
2024-05-24 11:55:35 +0200

    responsive mobile added

--- static/main.js
+++ static/main.js
@@ -1,5 +1,7 @@
 const menuTrigger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
 
 menuTrigger.addEventListener('click', (event) => {
-
+  menuTrigger.classList.toggle('active');
+  menu.classList.toggle('open');
 })
