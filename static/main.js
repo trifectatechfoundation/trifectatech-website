410d0e9131f28f1770bf8722cb401e4b801bd43d
Erwin <erwin@grafische-republiek.com>
2024-06-10 11:54:24 +0200

    darkmode added

--- static/main.js
+++ static/main.js
@@ -1,7 +1,31 @@
 const menuTrigger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.menu-panel');
 
 menuTrigger.addEventListener('click', (event) => {
   menuTrigger.classList.toggle('active');
   menu.classList.toggle('open');
 })
+
+const prefersColorSchemeTrigger = document.querySelector('.color-scheme');
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+
+prefersColorSchemeTrigger.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark');
+
+  if (localStorage.getItem('prefersColorScheme')) {
+    localStorage.removeItem('prefersColorScheme');
+    moon.style.display = 'block';
+    sun.style.display = 'none';
+  } else {
+    localStorage.setItem('prefersColorScheme', 'dark'); 
+    sun.style.display = 'block';
+    moon.style.display = 'none';
+  }
+});
+
+if (localStorage.getItem('prefersColorScheme')) {
+  document.documentElement.classList.add('dark');
+  sun.style.display = 'block';
+  moon.style.display = 'none';
+}
